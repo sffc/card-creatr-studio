a96c63b574444a9de9d5e4fdbe639e2260deaf3d
Shane F. Carr <shane@unicode.org>
2025-12-27 10:40:33 -1000

    Switch to js/all for stricter lint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -11,7 +11,7 @@ export default eslintConfig.defineConfig([
 			},
 		},
 		plugins: { js },
-		extends: ["js/recommended"],
+		extends: ["js/all"],
 		rules: {
 			"no-debugger": process.env.NODE_ENV === "production" ? 2 : 0,
 			"indent": ["error", "tab"],
@@ -20,6 +20,39 @@ export default eslintConfig.defineConfig([
 			"semi": ["error", "always"],
 			"no-console": "off",
 			"no-useless-catch": "off",
+
+			// Suppressions for js/all:
+			// Too pedantic:
+			"capitalized-comments": "off",
+			"func-style": "off",
+			"id-length": "off",
+			"max-lines": "off",
+			"max-lines-per-function": "off",
+			"max-params": "off",
+			"max-statements": "off",
+			"no-else-return": "off",
+			"no-magic-numbers": "off",
+			"one-var": "off",
+			"prefer-const": "off",
+			"prefer-exponentiation-operator": "off",
+			"prefer-object-has-own": "off",
+			"prefer-object-spread": "off",
+			"sort-keys": "off",
+			// Permissible style:
+			"curly": "off",
+			"no-alert": "off",
+			"no-inline-comments": "off",
+			"no-nested-ternary": "off",
+			"no-plusplus": "off",
+			"no-ternary": "off",
+			"no-underscore-dangle": "off",
+			// Warnings not errors:
+			"arrow-body-style": "warn",
+			"prefer-destructuring": "warn",
+			"prefer-template": "warn",
+			"vars-on-top": "warn",
+			// Re-enable after full ESM transition:
+			"strict": "off",
 		},
 	},
 	eslintConfig.globalIgnores([
